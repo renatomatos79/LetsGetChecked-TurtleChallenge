9909cc8033c7891c47dae39f3ee5c43c15f7a7f1
renato matos <renato.matos79@gmail.com>
2021-01-13 11:20:31 +0000

    loggin

---
+++ TurtleChallenge/Helper/SettingsHelper.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using TurtleChallenge.Assets.Contracts.Settings;
+using TurtleChallenge.Assets.Implementation.Settings;
+
+namespace TurtleChallenge.Helper
+{
+    public static class SettingsHelper
+    {
+        private static string GetJsonSettings(string[] args)
+        {
+            var fileName = string.Empty;
+            if (args.Contains("--settings="))
+            {
+                var settings = args.FirstOrDefault(f => f.StartsWith("--settings="));
+                var parts = settings.Split("=");
+                if (parts.Length == 2)
+                {
+                    if (System.IO.File.Exists(parts[1]))
+                    {
+                        fileName = System.IO.File.ReadAllText(parts[1]);
+                    }
+                }
+            }
+            if (string.IsNullOrEmpty(fileName))
+            {
+                fileName = System.IO.Path.Combine(Environment.CurrentDirectory, "Resources", "Settings", "default.json");
+            }
+            return System.IO.File.ReadAllText(fileName);
+        }
+
+        public static ISettings GetSettings(string[] args)
+        {
+            try
+            {
+                var json = GetJsonSettings(args);
+                return JsonConvert.DeserializeObject<DefaultSettings>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Oops! :(  there was a little problem during the game! please restart the game.");
+                Console.WriteLine($"ERROR => {ex}");
+                return null;
+            }
+        }
+    }
+}
--- TurtleChallenge/Program.cs
+++ TurtleChallenge/Program.cs
@@ -64,58 +64,36 @@ namespace TurtleChallenge
             return VALID_OPTIONS.Contains(opt) ? opt : GetMenu(engine);
         }
 
-        private static string GetJsonSettings(string[] args)
-        {
-            var fileName = string.Empty;
-            if (args.Contains("--settings="))
-            {
-                var settings = args.FirstOrDefault(f => f.StartsWith("--settings="));
-                var parts = settings.Split("=");
-                if (parts.Length == 2)
-                {
-                    if (System.IO.File.Exists(parts[1]))
-                    {
-                        fileName = System.IO.File.ReadAllText(parts[1]);
-                    }
-                }
-            }
-            if (string.IsNullOrEmpty(fileName))
-            {
-                fileName = System.IO.Path.Combine(Environment.CurrentDirectory, "Resources", "Settings", "default.json");
-            }            
-            return System.IO.File.ReadAllText(fileName);
-        }
-
-        private static ISettings GetSettings(string[] args)
-        {
-            var json = GetJsonSettings(args);
-            return JsonConvert.DeserializeObject<DefaultSettings>(json);
-        }
-
         static void Main(string[] args)
         {
-            var defaultSettings = GetSettings(args);
-            var json = JsonConvert.SerializeObject(defaultSettings);
-            System.IO.File.WriteAllText("c:\\temp\\settings.json", json);
-            Console.WriteLine(json);
-
-            var board = MapperHelper.Mapper.Map<DefaultBoard>(defaultSettings.BoardSettings);
-            var player = new Player(board);
-            var defaultSound = new DefaultSound();
-            var defaultEngine = new DefaultEngine(player, defaultSettings, defaultSound);
-            defaultEngine.Notify(Enums.EngineEvent.Play);
-
-            var opt = GetMenu(defaultEngine);
-            while (opt != EXIT)
+            var defaultSettings = SettingsHelper.GetSettings(args);
+            if (defaultSettings == null)
             {
-                var engineOperation = EngineOperationHelper.From(opt);
-                if (engineOperation.HasValue)
-                {
-                    defaultEngine.Do(engineOperation.Value);
-                }               
-                opt = GetMenu(defaultEngine);
+                Console.WriteLine("Oops! :(  The settings file could not be loaded!");
             }
-            
+            try
+            {
+                var board = MapperHelper.Mapper.Map<DefaultBoard>(defaultSettings.BoardSettings);
+                var player = new Player(board);
+                var defaultSound = new DefaultSound();
+                var defaultEngine = new DefaultEngine(player, defaultSettings, defaultSound);
+                defaultEngine.Notify(Enums.EngineEvent.Play);
+                var opt = GetMenu(defaultEngine);
+                while (opt != EXIT)
+                {
+                    var engineOperation = EngineOperationHelper.From(opt);
+                    if (engineOperation.HasValue)
+                    {
+                        defaultEngine.Do(engineOperation.Value);
+                    }
+                    opt = GetMenu(defaultEngine);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Oops! :(  there was a little problem during the game! please restart the game.");
+                Console.WriteLine($"ERROR => {ex}");
+            }
             Console.WriteLine("The end!");
         }
     }
